5d4e36ef60d011e8d95bbdf3c5fb933beee2aa08
vico724 <obdevic@gmail.com>
2024-11-03 16:46:03 +0100

    print stats

--- main.py
+++ main.py
@@ -3,13 +3,23 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{num_words} words found in the document")
+    print("")
+
+    sorted_dict = dict(sorted(chars_dict.items(), key=get_value, reverse=True))
+    for key in sorted_dict.keys():
+        if str(key).isalpha():
+            print(f"The {key} character was found {sorted_dict[key]} times")
+
     print(chars_dict)
 
+def get_value(item):
+    return item[1]
 
 def get_num_words(text):
     words = text.split()
     return len(words)
-
 
 def get_chars_dict(text):
     chars = {}
